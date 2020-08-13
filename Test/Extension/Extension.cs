291916f8f80f9be8c53f051387671e9b887a7bb0
Igbal032 <iqbal.hoff@list.ru>
2020-08-13 18:01:27 +0400

    first

---
+++ Test/Extension/Extension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public static class Extension
+    {
+        public static string calculate(double x, double y)
+        {
+            string result = "";
+            string error = "Biraz Sonra Cehd Edin";
+
+            if (x < 0 && y < 0)
+            {
+                x = -x;
+                y = -y;
+                result = "X equals to " + x.ToString() + " and Y equals to " + y.ToString();
+                return result;
+            }
+            else if ((x < 0 && y > 0) || (x > 0 && y < 0))
+            {
+                x = x + 0.5;
+                y = y + 0.5;
+
+                result = "X equals to " + x.ToString() + " and Y equals to " + y.ToString();
+                return result;
+            }
+            else if ((x > 0 && y > 0) && ((x < 0.5 || x > 2.0) && (y < 0.5 || y > 2.0)))
+            {
+                x = x / 10;
+                y = y / 10;
+
+                result = "X equals to " + x.ToString() + " and Y equals to " + y.ToString();
+                return result;
+            }
+            return error;
+
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("X ededin daxil edin: ");
+                double x = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Y ededin daxil edin: ");
+                double y = Double.Parse(Console.ReadLine());
+
+                string res = Extension.calculate(x, y);
+                Console.WriteLine("Netice :" + res);
+            }
+
+        }
+    }
+}
