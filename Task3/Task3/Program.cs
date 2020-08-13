fb4fef31beafb6da5c057bf136882ee8c6cf6457
Igbal032 <iqbal.hoff@list.ru>
2020-08-13 18:50:13 +0400

    3rd task added

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+            Console.Write("Birinci sözü daxil edin: ");
+            string firstWord = Console.ReadLine();
+            Console.Write("Ikinci sözü daxil edin: ");
+            string secondWord = Console.ReadLine();
+            Console.Write("Üçüncü sözü daxil edin: ");
+            string thirdWord = Console.ReadLine();
+            Console.Write("Dördüncü sözü daxil edin: ");
+            string fourthWord = Console.ReadLine();
+            words.Add(firstWord);
+            words.Add(secondWord);
+            words.Add(thirdWord);
+            words.Add(fourthWord);
+            var sort = words.OrderBy(n => n.Length);
+            Console.WriteLine(sort.FirstOrDefault()+" -- Qisa");
+            Console.WriteLine(sort.LastOrDefault()+" -- Uzun");
+        }
+    }
+}
